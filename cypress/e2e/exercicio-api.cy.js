cf8779fffbc20c515ecb27551d3c4b3d1c7a8e4e
regis rosa <regisrosa.silveira@hotmail.com>
2023-04-28 19:11:55 -0300

    exercicio-api

---
+++ cypress/e2e/exercicio-api.cy.js
@@ -0,0 +1,107 @@
+/// <reference types="cypress" />
+var faker =require('faker');
+
+describe('Testes da Funcionalidade Usuários', () => {
+
+    it('Deve listar usuários cadastrados', () => {
+     cy.request({
+        method: 'GET',
+        url: 'http://localhost:3000/usuarios'
+     }).then((response) =>{
+expect(response.body.usuarios[0].nome).to.equal('Heitor Monticelli Silveira')
+expect(response.status).to.equal(200)
+expect(response.body).to.have.property('usuarios')
+expect(response.duration).to.be.lessThan(15)
+     })
+    });
+
+    it('Deve cadastrar um usuário com sucesso', () => {
+     
+         cy.request({
+method: 'POST',
+url:'http://localhost:3000/usuarios',
+body: {
+     "nome":'Heitor Monticelli Silveira',
+     "email":(faker.internet.email()),
+     "password": "teste",
+     "administrador": "true"
+   }
+
+         })
+    });
+
+    it('Deve validar um usuário com email inválido', () => {
+              
+     cy.request({
+          method: 'POST',
+          url:'http://localhost:3000/usuarios',
+          body: {
+               "nome":'Heitor Monticelli Silveira',
+               "email":'Heitorfilho@ebac.com.br',
+               "password": "teste",
+               "administrador": "true"
+             },
+             failOnStatusCode: false
+                   }).then((response) =>{
+                    expect(response.status).to.equal(400)
+                   expect(response.body.message).to.equal('Este email já está sendo usado')
+                   })
+    });
+
+    it('Deve editar um usuário previamente cadastrado', () => {
+         cy.request('http://localhost:3000/usuarios').then(response =>{
+
+let id = (response.body.usuarios[0]._id)
+cy.request({
+     method:'PUT',
+     url:`http://localhost:3000/usuarios/${id}`,
+     body:{
+          "nome": "Fulano da Silva",
+          "email": "fulano@qa.com",
+          "password": "teste",
+          "administrador": "true"
+        }
+
+}).then(response =>{
+expect(response.body.message).to.equal('Registro alterado com sucesso')
+
+})
+
+         })
+    });
+
+    it('Deve deletar um usuário previamente cadastrado', () => {
+     cy.request('http://localhost:3000/usuarios').then(response =>{
+
+     let id = (response.body.usuarios[0]._id)
+     cy.request({
+          method:'PUT',
+          url:`http://localhost:3000/usuarios/${id}`,
+          body:{
+               "nome": "Fulano da Silva",
+               "email": "fulano@qa.com",
+               "password": "teste",
+               "administrador": "true"
+             }
+             
+             })
+             cy.request({
+               method:'DELETE',
+               url:`http://localhost:3000/usuarios/${id}`,
+          }).then(response =>{
+expect(response.body.message).to.equal('Registro excluído com sucesso')
+expect(response.status).to.equal(200)
+
+          })
+     
+     })
+
+
+
+
+
+
+    });
+
+
+});
