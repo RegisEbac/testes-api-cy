94e152b4ef8784faa7cdea1fff2764e976f4c3e7
Regis da rosa silveira <113696689+RegisEbac@users.noreply.github.com>
2023-05-08 04:07:32 -0300

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,7 @@
 module.exports = {
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'mochawesome-report',
-    overwrite: false,
-    html: false,
-    json: true,
-  },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000/',
   },
-}
+};
